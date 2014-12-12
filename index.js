22b8a456216b8f7ecad4785f73caf41cdd943aec
AtsushiIzumihara <Atsushi.Izumihara@jp.sony.com>
2014-12-12 18:23:05 +0900

    index.htmlをコントローラとライトに振り分ける口とした　/ lightが排他的に名乗って作られ、其れが死ぬのと同時にcontrollerにも死んだことが通達され、管理上も消える。

--- index.js
+++ index.js
@@ -6,33 +6,68 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/controller', function(req, res){
+  res.sendFile(__dirname + '/controller.html');
+});
+
+app.get('/light', function(req, res){
+  res.sendFile(__dirname + '/light.html');
+});
+
 var count = 0;  // 人数
-var roomlist = {};
+var lightlist = {};
 
 io.on('connection', function(socket){
   count++;
   console.log('a user connected: ' + count);
 
   socket.on('disconnect', function(){
+    console.log(socket.rooms);
     count--;
     console.log('user disconnected: ' + count);
   });
 
-  socket.on('enter', function(enterdata){
-    var roomname = enterdata.value;
-    if(!roomlist[roomname]){
-      console.log('no such room');
-      roomlist[roomname] = 1;
+  // from light
+  socket.on('make light', function(data){
+    var lightname = data.value;
+    if(!lightlist[lightname]){
+      console.log('no such light');
+      lightlist[lightname] = 1;
+      socket.join(lightname);
     } else {
       console.log('already exist');
-      roomlist[roomname]++;
+      socket.emit('light already exist', {msg: 'the light name is already exist'});
     }
-    socket.join(roomname);
+  });
+
+  socket.on('destroy light', function(data){
+    var lightname = data.value;
+    console.log('destroy light');
+    delete lightlist[lightname];
+    console.log(lightlist);
+
+    io.to(socket.rooms[1]).emit('destroyed');
+  });
+
+  // from controller
+  socket.on('join light', function(data){
+    var lightname = data.value;
+    if(!lightlist[lightname]){
+      console.log('no such light');
+    } else {
+      console.log('find light');
+      socket.join(lightname);
+    }
+  });
+
+  socket.on('leave light', function(){
+    console.log("leave:", socket.rooms[1]);
+    socket.leave(socket.rooms[1]);
   });
 
   socket.on('chat message', function(msg){
     // console.log(socket.rooms);
-    var room = socket.rooms[1];
+    var room = socket.rooms[1]; // 0 is default, 1 is the first.
     io.to(room).emit('chat message', msg);
   });
 });
